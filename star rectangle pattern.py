f1ce7c71bde7f9cf74f34b7f7af0936d48675c85
Obed John <73648003+osjohn01@users.noreply.github.com>
2022-12-30 09:22:15 +0530

    Add files via upload

---
+++ star rectangle pattern.py
@@ -0,0 +1,6 @@
+l=int(input("ENTER THE LENGTH FOR RECTANGLE PATTERN --> "))
+b=int(input("ENTER THE BREADTH FOR RECTANGLE PATTERN --> "))
+for i in range(b):
+    for j in range(l):
+        print("*",end=" ")
+    print()
---
+++ star square pattern.py
@@ -0,0 +1,5 @@
+l=int(input("ENTER THE SIDE FOR SQUARE PATTERN --> "))
+for i in range(l):
+    for j in range(l):
+        print("*",end=" ")
+    print()
