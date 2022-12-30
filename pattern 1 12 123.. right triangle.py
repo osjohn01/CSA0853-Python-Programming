ead6922ae983c9357941aee1406323e6e1919cf6
Obed John <73648003+osjohn01@users.noreply.github.com>
2022-12-30 09:03:14 +0530

    Add files via upload

---
+++ pattern 1 12 123.. right triangle.py
@@ -0,0 +1,5 @@
+n=int(input("ENTER THE VALUE OF N FOR PATTERN --> "))
+for i in range(n+1):
+    print()
+    for j in range(1,i+1):
+        print(j,end=" ")
---
+++ star pattern right triangle.py
@@ -0,0 +1,5 @@
+n=int(input("ENTER THE VALUE OF N FOR PATTERN --> "))
+for i in range(n+1):
+    print()
+    for j in range(i):
+        print("*",end=" ")
