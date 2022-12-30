45102c7d2f5c0c23ddaa5b334c4a1653210ee889
Obed John <73648003+osjohn01@users.noreply.github.com>
2022-12-30 08:38:48 +0530

    Update solving quadratic equation.py

--- solving quadratic equation.py
+++ solving quadratic equation.py
@@ -1,8 +1,9 @@
+import cmath as m
 a=int(input("ENTER VALUE OF A --> "))
 b=int(input("ENTER VALUE OF B --> "))
 c=int(input("ENTER VALUE OF C --> "))
 print("THE QUADRATIC EQUATION IS --> ",a,"x² +",b,"x +",c)
-d=b**2+4*a*c
+d=b**2-4*a*c
 if d==0:
     print("ROOTS ARE REAL AND EQUAL")
 elif d>0:
@@ -10,7 +11,7 @@ elif d>0:
 else:
     print("ROOTS ARE IMAGINARY")
 
-x1=((-1*b)+d**0.5)/(2*a)
-x2=((-1*b)-d**0.5)/(2*a)
+x1=((-1*b)+m.sqrt(d))/(2*a)
+x2=((-1*b)-m.sqrt(d))/(2*a)
 print("THE FIRST ROOT IS ",x1)
 print("THE SECOND ROOT IS ",x2)
