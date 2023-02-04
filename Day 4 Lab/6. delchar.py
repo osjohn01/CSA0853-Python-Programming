74baa31a886513e7c203f00539cac1fbab2ea3f2
Obed John <73648003+osjohn01@users.noreply.github.com>
2023-02-04 14:44:21 +0530

    Add files via upload

---
+++ Day 4 Lab/6. delchar.py
@@ -0,0 +1,9 @@
+def delchar(a,b):
+    c=len(a)
+    d=""
+    for i in range(0,c):
+        if a[i] not in b:
+            d+=a[i]
+    print("String after deletion :",d)
+a=input("Enter a string:")
+b=input("Enter character to be deleted :")
